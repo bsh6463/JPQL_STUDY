68ce2a961acae5d2fa0c7ce44a7279b83ec9938e
bsh6463 <bsh9510@gmail.com>
2021-08-16 20:03:21 +0900

    210816 FETCH JOIN1

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -15,27 +16,43 @@ public class JpaMain {
 
         try{
 
-                Member member =  new Member();
-                member.setAge(10);
-                member.setUsername("관리자");
-                member.setType(MemberType.ADMIN);
+              Team teamA = new Team();
+              teamA.setName("팀A");
+              em.persist(teamA);
 
-                Team team  = new Team();
-                team.setName("team");
-                em.persist(team);
+              Team teamB = new Team();
+              teamB.setName("팀B");
+              em.persist(teamB);
 
-                member.changeTeam(team);
-                em.persist(member);
+              Member member1 = new Member();
+              member1.setUsername("회원1");
+              member1.setTeam(teamA);
+              em.persist(member1);
 
+              Member member2 = new Member();
+              member2.setUsername("회원2");
+              member2.setTeam(teamA);
+              em.persist(member2);
+
+              Member member3 = new Member();
+              member3.setUsername("회원3");
+              member3.setTeam(teamB);
+              em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select size(t.members) from Team t ";
-            List<Integer> resultList = em.createQuery(query, Integer.class).getResultList();
+            String query = "select distinct t From Team t join fetch t.members";
 
-            for (Integer s : resultList) {
-                System.out.println("s = " + s);
+            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+            System.out.println("resultList.size() = " + resultList.size());
+            for (Team team : resultList) {
+                System.out.println("team.getName() = " + team.getName()+"  team.getMembers().size() : " + team.getMembers().size());
+
+                for(Member member : team.getMembers()){
+                    System.out.println("--> member = " + member);
+                }
+
             }
 
 
